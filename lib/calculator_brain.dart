deb31bb6393efdf8811602813e69ccbb69d94533
Yao Josué <jyao2000@yahoo.fr>
2020-05-21 17:19:17 +0200

    fin calcul

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -23,4 +23,14 @@ class CalculatorBrain{
       return 'Underweight';
     }
   }
+
+  String getInterpretation(){
+    if(_bmi>=25){
+      return 'You have a higher than normal body weight. try to exercice more.';
+    } else if (_bmi > 18.5) {
+      return 'You have a normal body weight. Good job';
+    } else {
+      return 'You have a lower than normal body weight. You can eat a bit more.';
+    }
+  }
 }
