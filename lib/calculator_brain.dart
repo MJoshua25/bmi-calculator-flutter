c00ba8e50125144be46dcd6e8521dac01b0be17a
Yao Josué <jyao2000@yahoo.fr>
2020-05-21 17:15:36 +0200

    fin calcul

--- lib/calculator_brain.dart
+++ lib/calculator_brain.dart
@@ -11,4 +11,14 @@ class CalculatorBrain{
     double bmi = weight / pow(height/100, 2);
     return bmi.toStringAsFixed(1);
   }
+
+  String getResult(){
+    if(bmi>=25){
+      return 'Overweight';
+    } else if (bmi > 18.5) {
+      return 'normal';
+    } else {
+      return 'Underweight';
+    }
+  }
 }
